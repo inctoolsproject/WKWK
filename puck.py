87a7c1a1be9042303df06544f204e7f60749e7a6
rinda18 <41496540+rinda18@users.noreply.github.com>
2018-07-24 20:12:00 +0700

    Update puck.py

--- puck.py
+++ puck.py
@@ -1270,8 +1270,8 @@ def clientBot(op):
                                     #ret_ += "\n  Version : Premium"
                                     #ret_ += "\n  Creator : {}".format(creator.displayName)
                                     #ret_ += "\n  Creator : @!".format(Owner)
-                                    ret_ += "\n╚═〘 Creator : @!〙"
-                                    client.sendMessage(to, str(ret_), [Ownerz])
+                                    ret_ += "\n╚═〘 READ TEXT BELOW 〙"
+                                    client.sendMessage(to, str(ret_))
                                     #client.sendMessage(to, "「 Read Text Below 」")
                                     sendMention(to, "「 About Rinda 」\n\nThe Beginning of this Bot Comes from Helloworld, I'm just Reworked This!\n\nOf Course Special Thanks To HelloWorld, And the Friends Around Me!\n\n*Creator : @!", [Ownerz])
                                 except Exception as e:
